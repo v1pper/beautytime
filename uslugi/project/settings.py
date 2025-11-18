9b69463ccbf4a48a253667832f66e35def640220
v1pper <vova22089@gmail.com>
2025-11-18 19:15:20 +0300

    Add Railway configuration and Django project structureeууа

--- uslugi/project/settings.py
+++ uslugi/project/settings.py
@@ -23,7 +23,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+   
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -103,3 +103,6 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ]
 }
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
